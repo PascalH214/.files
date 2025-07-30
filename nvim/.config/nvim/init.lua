ff524e4f6f199cc956b569db3ad3cf116b4bf922
PascalH214 <pascal02012004@freenet.de>
2025-07-30 21:26:10 +0200

    Add completion and diagnostic

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,29 @@
 require("config.lazy")
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 
 vim.cmd.colorscheme "catppuccin-mocha"
+
+vim.diagnostic.config({
+	virtual_text = {
+    severity = {
+			min = vim.diagnostic.severity.ERROR
+		},
+  },
+	virtual_lines = {
+		current_line = false,
+    severity = {
+			min = vim.diagnostic.severity.ERROR
+		},
+	},
+})
+
+vim.keymap.set("n", "<leader>d", function()
+  vim.diagnostic.open_float(0, {
+    scope = "line",
+    severity_sort = true,
+  })
+end, { desc = "Show diagnostics for current line" })
+
