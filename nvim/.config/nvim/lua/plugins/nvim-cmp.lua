e3b9c003d9fc5b78c0ad18d3caee8148f6b2f496
PascalH214 <pascal02012004@freenet.de>
2025-07-30 23:04:18 +0200

    Fix cmp

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,11 +1,11 @@
 return {
-	'neovim/nvim-lspconfig',
+	'hrsh7th/nvim-cmp',
 	dependencies = {
+		'neovim/nvim-lspconfig',
 		'hrsh7th/cmp-nvim-lsp',
 		'hrsh7th/cmp-buffer',
 		'hrsh7th/cmp-path',
 		'hrsh7th/cmp-cmdline',
-		'hrsh7th/nvim-cmp',
 		'saadparwaiz1/cmp_luasnip',
 		
 		'L3MON4D3/LuaSnip'
