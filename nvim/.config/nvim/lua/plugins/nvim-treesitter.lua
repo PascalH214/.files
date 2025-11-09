37d6223ed1f1531a90071142e3ee171007c49b25
PascalH214 <pascal02012004@freenet.de>
2025-11-09 20:07:13 +0100

    Add nvim-treesitter (and terraform)

---
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,19 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = 'master',
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter").setup {
+			ensure_installed = {
+				"terraform",
+				"hcl",
+			},
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+			}
+		}
+	end,
+}
